743a6cfd88c6525a55eb09496bcd6935d26fc44d
Jovelasirot <jovelasirot@gmail.com>
2024-03-25 10:15:11 +0100

    initila classes layout

---
+++ src/main/java/bw4_team1/Application.java
@@ -0,0 +1,19 @@
+package bw4_team1;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("atm");
+    public static void main(String[] args) {
+
+        EntityManager eM = emf.createEntityManager();
+
+        System.out.println("Hello World!");
+
+
+        emf.close();
+        eM.close();
+    }
+}
