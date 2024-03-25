139273e8e9472bd36292326ffee62ce511484f7f
GiulioChiarenza <giulio.chiarenza@outlook.it>
2024-03-25 14:47:22 +0100

    creazione classe Pass

--- src/main/java/entities/Pass.java
+++ src/main/java/entities/Pass.java
@@ -1,4 +1,77 @@
 package entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Pass")
 public class Pass {
+    @Id
+    @GeneratedValue
+    private long id;
+    private PassDuration passDuration;
+    private int CardNumber;
+    private int IssueDate;
+    @ManyToOne
+    private long IssueId;
+
+
+    public Pass( PassDuration passDuration, int cardNumber, int issueDate, long issueId) {
+
+        this.passDuration = passDuration;
+        CardNumber = cardNumber;
+        IssueDate = issueDate;
+        IssueId = issueId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public PassDuration getPassDuration() {
+        return passDuration;
+    }
+
+    public void setPassDuration(PassDuration passDuration) {
+        this.passDuration = passDuration;
+    }
+
+    public int getCardNumber() {
+        return CardNumber;
+    }
+
+    public void setCardNumber(int cardNumber) {
+        CardNumber = cardNumber;
+    }
+
+    public int getIssueDate() {
+        return IssueDate;
+    }
+
+    public void setIssueDate(int issueDate) {
+        IssueDate = issueDate;
+    }
+
+    public long getIssueId() {
+        return IssueId;
+    }
+
+    public void setIssueId(long issueId) {
+        IssueId = issueId;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Pass{" +
+                "id=" + id +
+                ", passDuration=" + passDuration +
+                ", CardNumber=" + CardNumber +
+                ", IssueDate=" + IssueDate +
+                ", IssueId=" + IssueId +
+                '}';
+    }
 }
