ccc639f994c3e132ba9538876e7fb797dd1455d6
GiulioChiarenza <giulio.chiarenza@outlook.it>
2024-03-26 10:39:37 +0100

    Pass DAO e PassTest

--- src/main/java/entities/Pass.java
+++ src/main/java/entities/Pass.java
@@ -11,7 +11,8 @@ public class Pass {
     private long Pass_id;
     @Enumerated(EnumType.STRING)
     private PassDuration passDuration;
-    private int CardNumber;
+    @ManyToOne
+    private Sales Isssue_id;
     private int IssueDate;
     @ManyToOne
     @JoinColumn(name = "card_number")
@@ -21,19 +22,20 @@ public class Pass {
     public Pass() {
     }
 
-    public Pass(PassDuration passDuration, int cardNumber, int issueDate) {
+    public Pass( PassDuration passDuration,  int issueDate) {
 
         this.passDuration = passDuration;
-        CardNumber = cardNumber;
+
         IssueDate = issueDate;
+
     }
 
-    public long getId() {
+    public long getPass_id() {
         return Pass_id;
     }
 
-    public void setId(long id) {
-        this.Pass_id = id;
+    public void setPass_id(long pass_id) {
+        Pass_id = pass_id;
     }
 
     public PassDuration getPassDuration() {
@@ -44,12 +46,12 @@ public class Pass {
         this.passDuration = passDuration;
     }
 
-    public int getCardNumber() {
-        return CardNumber;
+    public Sales getIsssue_id() {
+        return Isssue_id;
     }
 
-    public void setCardNumber(int cardNumber) {
-        CardNumber = cardNumber;
+    public void setIsssue_id(Sales isssue_id) {
+        Isssue_id = isssue_id;
     }
 
     public int getIssueDate() {
@@ -60,14 +62,22 @@ public class Pass {
         IssueDate = issueDate;
     }
 
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
 
     @Override
     public String toString() {
         return "Pass{" +
-                "id=" + Pass_id +
+                "Pass_id=" + Pass_id +
                 ", passDuration=" + passDuration +
-                ", CardNumber=" + CardNumber +
+                ", Isssue_id=" + Isssue_id +
                 ", IssueDate=" + IssueDate +
+                ", card=" + card +
                 '}';
     }
 }
