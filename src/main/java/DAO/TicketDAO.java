4dd03abe7b08061ec6043b59ca0acecdb9a99097
Roberta Stira <151186486+robertastira@users.noreply.github.com>
2024-03-27 12:22:54 +0100

    ticketvalidation period of time

--- src/main/java/DAO/TicketDAO.java
+++ src/main/java/DAO/TicketDAO.java
@@ -78,4 +78,13 @@ public class TicketDAO {
             return "The ticket hasn't already been VALIDATED";
         }
     }
+    public long countValidatedTicketsByVehicleIdAndPeriod(int vehicleId, LocalDate startDate, LocalDate endDate) {
+        TypedQuery<Long> query = em.createQuery(
+                "SELECT COUNT(t) FROM Ticket t WHERE t.sales.vehicle.id = :vehicleId AND t.validation = :validatedStatus AND t.sales.date >= :startDate AND t.sales.date <= :endDate", Long.class);
+        query.setParameter("vehicleId", vehicleId);
+        query.setParameter("validatedStatus", Validation.VALIDATED);
+        query.setParameter("startDate", startDate);
+        query.setParameter("endDate", endDate);
+        return query.getSingleResult();
+    }
 }
